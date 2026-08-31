5c16c7635cba6e4723c56064e8428b18d96fe7c3
garciafreesia <garciafreesia@gmail.com>
2026-08-31 15:26:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main() {
   std::cout << "In 3 years, I hope to have graduated with a Bachelor's Degree in Data Sciecnce.\n";
   std::cout << "In 5 years, I want to have a good paying job as a Healthcare Data Analyst.\n";
   std::cout << "In 7 years, I hope to have a successful career and my own house.\n";
-  std:cout << "\nMy coding goal is " << years << "year(s).\n";
+  std::cout << "\nMy coding goal is " << years << "year(s).\n";
 
   return 0;
 }
